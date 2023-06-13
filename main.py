2fba241b72136a6df87ce14c143e034c76e924c5
BuildTools <unconfigured@null.spigotmc.org>
2023-06-13 12:45:20 +0200

    init setup neural network class

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 import numpy
+# scipy.special for the sigmoid function expit()
 import scipy.special
+# library for plotting arrays
 import matplotlib.pyplot
 
 
-class NeuralNetwork:
+# neural network class definition
+class neuralNetwork:
 
+    # initialise the neural network
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
         self.inodes = inputnodes
-        self.hnodes = hiddennodes,
+        self.hnodes = hiddennodes
         self.onodes = outputnodes
 
+        # learning rate
         self.lr = learningrate
+        pass
 
-        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
-
-        print("Weight-Input-Hidden (wih):")
-        print(self.wih)
-        print("Weight-Hidden-Output (who):")
-        print(self.who)
-
+    # train the neural network
     def train(self):
-        print('training')
+        print("Please train me...")
+        pass
 
+    # query the neural network
     def query(self):
-        print("I ask...")
+        print("Please aks me...")
+        pass
 
 
+# number of input, hidden and output nodes
 input_nodes = 3
 hidden_nodes = 3
 output_nodes = 3
 
+# learning rate is 0.3
 learning_rate = 0.3
 
-n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+# create instance of neural network
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
+# train the neural network
 n.train()
 
+# query the network
 outputs = n.query()
-
 
 print("performance = great")
